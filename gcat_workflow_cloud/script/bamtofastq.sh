e361ce5a33519186b95e308ebd73490377bdd5cb
aokad <aokada@ncc.go.jp>
2021-03-19 11:56:47 +0900

    refine bamtofastq.sh

--- gcat_workflow_cloud/script/bamtofastq.sh
+++ gcat_workflow_cloud/script/bamtofastq.sh
@@ -5,6 +5,8 @@ set -o nounset
 set -o pipefail
 set -x
 
+export LD_LIBRARY_PATH=/usr/local/lib
+
 F1_NAME=${OUTPUT_DIR}"/1_1.fastq"
 F2_NAME=${OUTPUT_DIR}"/1_2.fastq"
 O1_NAME=${OUTPUT_DIR}'/unmatched_first_output.txt'
@@ -13,14 +15,11 @@ T=${OUTPUT_DIR}'/temp.txt'
 S=${OUTPUT_DIR}'/single_end_output.txt'
 
 mkdir -p ${OUTPUT_DIR}
-
-export LD_LIBRARY_PATH=/usr/local/lib
+touch ${F1_NAME} ${F2_NAME} ${T} ${S} ${O1_NAME} ${O2_NAME}
 
 for NUM in `seq 0 ${SAMPLE_MAX_INDEX}`
 do
     INPUT_BAM="INPUT_BAM_${NUM}"
-
-    touch ${F1_NAME} ${F2_NAME} ${T} ${S} ${O1_NAME} ${O2_NAME}
 
     /usr/local/bin/bamtofastq ${PARAM} filename=${!INPUT_BAM} F=${F1_NAME}.tmp F2=${F2_NAME}.tmp T=${T}.tmp S=${S}.tmp O=${O1_NAME}.tmp O2=${O2_NAME}.tmp
     if [ -s ${F1_NAME}.tmp ]; then
@@ -32,7 +31,7 @@ do
     elif [ -s ${S}.tmp ]; then
         cat ${S}.tmp >> ${F1_NAME}
     fi
-    if [ -s ${T}.tmp ]; then        
+    if [ -s ${T}.tmp ]; then
         cat ${T}.tmp >> ${T}
     fi
     if [ -s ${O1_NAME}.tmp ]; then
